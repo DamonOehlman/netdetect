e2db138b5ceb186ee0728c0c42fa27c803b298f2
Damon Oehlman <damon.oehlman@gmail.com>
2015-05-24 03:53:46 +1000

    use chrome app system.network to discover network capcabilities

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 var storage = require('chromeapp/storage')();
 var manifest = require('chromeapp/manifest')();
+
+function handleInterfaceDiscovery(interfaces) {
+  var networks = interfaces.filter(isIPv4).map(function(data) {
+    return data.address + '/' + data.prefixLength;
+  });
+  
+  console.log(networks);
+}
+
+function isIPv4(data) {
+  // ensure we do not have : separated parts
+  return data.address.split(':').length <= 1;
+}
+
+chrome.system.network.getNetworkInterfaces(handleInterfaceDiscovery);
